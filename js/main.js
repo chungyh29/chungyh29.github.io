24dde9bbb46ad62961ac067cde23195ac62e99e9
Franco <chungyh29@yahoo.com>
2021-04-19 14:16:51 +0900

    changed appearance

--- js/main.js
+++ js/main.js
@@ -1,70 +1,111 @@
-$(document).ready(function() {
-	$(".item-list li").mouseenter(function(){  
-        $(this).find($('.item-list .hover')).stop(true, true).fadeIn(600);
-        return false;
-     });
-      $('.item-list li').mouseleave(function(){  
-        $(this).find($('.item-list .hover')).stop(true, true).fadeOut(400);
-        return false;
-     });
-      jQuery(document).on('click', ".menu_trigger", function (e) {
-        e.preventDefault()
-        window.setTimeout(function() {
-            if(jQuery('#nav').hasClass('clicked')){
-                jQuery('#nav').stop(true,true).animate({height:'hide'},100);
-                jQuery('#nav').removeClass('clicked');
-            }else{
-                jQuery('#nav').stop(true,true).animate({height:'show'},400);
-                jQuery('#nav').addClass('clicked');
-            }
-        }, 400);
-        return false;
-    });
-    jQuery("#nav").on('click', '.drops', function () {
-        if (jQuery(this).hasClass("active")) {
-            jQuery(this).removeClass("active").parent().next().slideUp();
-        } else {
-            jQuery(this).addClass("active").parent().next().slideDown();
-        }
-        return false;
-    });
-// begin add	
-	var $container = $('#container');
-	// init
-	$container.isotope({
-		// options
-		itemSelector: '.item',
-		layoutMode: 'cellsByRow',
-		cellsByRow: {
-			columnWidth: 295,
-			rowHeight: 295
-		}
-		});
-		
-	$('#filters').on( 'click', 'li', function() {
-		var filterValue = $(this).attr('data-filter');
-		$container.isotope({ filter: filterValue });
-		$( "#filters li" ).removeClass("active");
-		$(this).addClass("active");
-	});
-	$('.fancybox').fancybox({
-	  helpers: {
-	    overlay: {
-	      locked: false
-	    }
-	  }
-	});
-// end add	
-}); 
-$(window).resize(function() {
-    if($(document).width() > 768){
-      $( "#nav" ).addClass("active");
-      $( "#nav ul" ).attr('style','');
-      $( "#nav" ).attr('style','');
-      $( "#nav" ).removeClass("clicked");
-      $( "#nav .active" ).removeClass('active');
+  const getRandomColour = (x) => {
+    const colours = ['rgb(233,23,90)', 'rgb(241,100,42)','rgb(1,211,191)','rgb(102,11,177)','rgb(215,215,0)','rgb(30,30,250)'];
+    const colours2 = ['rgb(255,63,130)', 'rgb(255,140,102)','rgb(41,251,231)','rgb(142,51,207)','rgb(255,255,40)','rgb(70,70,255)'];
+
+    const backgroundColours = ['rgb(252,208,207)','rgb(253,251,213)','rgb(213,251,200)','rgb(219,250,255)','rgb(236,211,253)'];
+
+    let colourIndex = 0
+
+    if(x == 1){
+      
+      colourIndex = Math.ceil(Math.random() * colours.length );
+      console.log(colourIndex-1)
+      return {colour1: colours[colourIndex-1], colour2: colours2[colourIndex-1]}
+    }else{
+      colourIndex = Math.ceil(Math.random() * backgroundColours.length );
+      return backgroundColours[colourIndex-1];
+    } ;
+  }
+
+  const getRandomShape = () => {
+    const shapes = ['circle', 'rectangle'];
+    const shapeIndex = Math.ceil(Math.random() * shapes.length);
+
+    return shapes[shapeIndex-1];
+  }
+  const createShape = (shapeName) => {
+    const lowShapeName = shapeName.toLowerCase();
+    const shapeElement = document.createElement('DIV');
+    const {colour1, colour2} = getRandomColour(1);
+   
+    //Get window height and width
+    const winHeight = window.innerHeight;
+    const winWidth = window.innerWidth;
+
+    maxVal = Math.min(winHeight, winWidth)*0.4;
+    minVal = 50;
+
+    //FOr rectangles, get random number from minVal and 40% of window's shorter side
+    const size1 = Math.floor(Math.random() * maxVal + minVal);
+    const size2 = Math.floor(Math.random() * maxVal + minVal);
+
+      //For circles, get random number from minVal and 30% of window's shoerter side
+    const size3 = Math.floor(Math.random() * maxVal + minVal);
+    //Get a random rotation
+    const rot = Math.random() * 360;
+
+    //Change opacity level
+    shapeElement.style.opacity=0.9;
+    switch(lowShapeName){
+      case 'circle':
+        shapeElement.style.background = `radial-gradient(circle at 50px 50px,${colour1}, #111`;
+        shapeElement.style.width = `${size3}px`;
+        shapeElement.style.height = `${size3}px`;
+        shapeElement.style.borderRadius = '50%';
+        break;
+      case 'rectangle':
+        shapeElement.style.background = `linear-gradient(to right, ${colour1}, ${colour2}, ${colour1})`;
+        shapeElement.style.width = `${size1}px`;
+        shapeElement.style.height = `${size2}px`;
+        break;
+      case 'triangle':
+        shapeElement.style.width = 0;
+        shapeElement.style.height = 0;
+        shapeElement.style.borderLeft = `${size1}px solid transparent`;
+        shapeElement.style.borderRight = `${size1}px solid transparent`;
+        shapeElement.style.borderBottom = `${size1*2}px solid ${colour}`;
+        shapeElement.style.transform = `rotate(${rot}deg)`
+        break;
+      
     }
-    else {
-        $( "#nav" ).removeClass("active");
+    return shapeElement;
+  }
+
+
+  const changeBackground = () => {
+    const wrapperDiv = document.querySelector('.wrapper-holder');
+    wrapperDiv.style.backgroundColor = getRandomColour(0);
+
+    //Create instances of a shape with random dimensions and located randomly
+    const shape = getRandomShape();
+    console.log(shape)
+    var shapeEle = null;
+    const numShapes = 25;
+
+    //Get a random location for the shape
+    //window is divided in a grid of equal number of rows and columns. A shape will be added to each square of grid. 
+    const rowCol = Math.floor(Math.sqrt(numShapes))
+
+    //Window is divided in a grid of rows and columns and a shape is placed randomly somewhere in a grid square
+    let rowLen=0, colLen = 0;
+    let loc1=0, loc2 = 0;
+    let size=0;
+    for(let i=0; i<rowCol; i++){
+      rowLen = i*100/rowCol;
+      loc1 = Math.random() * rowLen;
+
+      for(let j=0; j<rowCol; j++ ){
+        colLen = j*100/rowCol;
+        loc1 =  Math.random() * 100/rowCol + rowLen - 50/rowCol;
+        loc2 = Math.random() * 100/rowCol + colLen - 50/rowCol;
+      
+        shapeEle = createShape(shape);
+        shapeEle.style.position = 'absolute';
+        shapeEle.style.top = `${loc1}%`;
+        shapeEle.style.left = `${loc2}%`;
+
+        wrapperDiv.insertBefore(shapeEle, wrapperDiv.childNodes[0]);
+      }
     }
-});
+}
+changeBackground();
